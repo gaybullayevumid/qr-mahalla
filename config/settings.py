c7242addaefb4006981782a523f3b5c0cfb88e26
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-12-17 18:22:52 +0500

    Update static file settings to change STATIC_URL to '/staticfiles/' and set STATIC_ROOT to 'staticfiles'

--- config/settings.py
+++ config/settings.py
@@ -131,6 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'

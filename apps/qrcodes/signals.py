80a5634e1d874a103141781c8e3d15a37440878a
gaybullayevumid <umidgaybullayev955@gmail.com>
2026-01-09 22:20:07 +0500

    Disable all auto generation - QR codes and houses created manually by admin

--- apps/qrcodes/signals.py
+++ apps/qrcodes/signals.py
@@ -34,6 +34,7 @@ def create_qr_code_for_house(sender, instance, created, **kwargs):
 def maintain_unclaimed_houses(sender, instance, created, **kwargs):
     """
     Maintain minimum number of unclaimed houses.
+    DISABLED: Admin manages houses manually now.
 
     When a house gets an owner, check unclaimed houses count
     and create new ones if needed to maintain the minimum.
@@ -44,34 +45,37 @@ def maintain_unclaimed_houses(sender, instance, created, **kwargs):
         created: Boolean indicating if this is a new object
         **kwargs: Additional keyword arguments
     """
-    if created or not instance.owner:
-        return
+    # Disabled - admin manages houses manually
+    pass
+    # if created or not instance.owner:
+    #     return
 
-    unclaimed_count = House.objects.filter(owner__isnull=True).count()
+    # unclaimed_count = House.objects.filter(owner__isnull=True).count()
 
-    if unclaimed_count < MINIMUM_UNCLAIMED_HOUSES:
-        houses_needed = MINIMUM_UNCLAIMED_HOUSES - unclaimed_count
-        mahalla = instance.mahalla
+    # if unclaimed_count < MINIMUM_UNCLAIMED_HOUSES:
+    #     houses_needed = MINIMUM_UNCLAIMED_HOUSES - unclaimed_count
+    #     mahalla = instance.mahalla
 
-        for i in range(houses_needed):
-            house_count = House.objects.filter(mahalla=mahalla).count()
-            House.objects.create(
-                mahalla=mahalla,
-                address=f"{mahalla.name}, avtomatik yaratilgan uy",
-                house_number=f"AUTO-{house_count + i + 1}",
-                owner=None,
-            )
+    #     for i in range(houses_needed):
+    #         house_count = House.objects.filter(mahalla=mahalla).count()
+    #         House.objects.create(
+    #             mahalla=mahalla,
+    #             address=f"{mahalla.name}, avtomatik yaratilgan uy",
+    #             house_number=f"AUTO-{house_count + i + 1}",
+    #             owner=None,
+    #         )
 
-        logger.info(
-            f"Created {houses_needed} new unclaimed houses to maintain "
-            f"minimum of {MINIMUM_UNCLAIMED_HOUSES}"
-        )
+    #     logger.info(
+    #         f"Created {houses_needed} new unclaimed houses to maintain "
+    #         f"minimum of {MINIMUM_UNCLAIMED_HOUSES}"
+    #     )
 
 
 @receiver(post_save, sender=QRCode)
 def maintain_unclaimed_qrcodes(sender, instance, created, **kwargs):
     """
     Maintain minimum number of unclaimed QR codes.
+    DISABLED: Admin creates QR codes manually now.
 
     When a QR code gets claimed (linked to a house), automatically
     generate a new unclaimed QR code to maintain the pool of 10.
@@ -82,25 +86,27 @@ def maintain_unclaimed_qrcodes(sender, instance, created, **kwargs):
         created: Boolean indicating if this is a new object
         **kwargs: Additional keyword arguments
     """
-    # Skip if this is a new QR code being created
-    if created:
-        return
+    # Disabled - admin creates QR codes manually
+    pass
+    # # Skip if this is a new QR code being created
+    # if created:
+    #     return
 
-    # Only act when QR code gets claimed (house assigned)
-    if not instance.house:
-        return
+    # # Only act when QR code gets claimed (house assigned)
+    # if not instance.house:
+    #     return
 
-    # Check unclaimed QR codes count
-    unclaimed_count = QRCode.objects.filter(house__isnull=True).count()
+    # # Check unclaimed QR codes count
+    # unclaimed_count = QRCode.objects.filter(house__isnull=True).count()
 
-    if unclaimed_count < MINIMUM_UNCLAIMED_QRCODES:
-        qrcodes_needed = MINIMUM_UNCLAIMED_QRCODES - unclaimed_count
+    # if unclaimed_count < MINIMUM_UNCLAIMED_QRCODES:
+    #     qrcodes_needed = MINIMUM_UNCLAIMED_QRCODES - unclaimed_count
 
-        # Create new unclaimed QR codes
-        for i in range(qrcodes_needed):
-            QRCode.objects.create()
+    #     # Create new unclaimed QR codes
+    #     for i in range(qrcodes_needed):
+    #         QRCode.objects.create()
 
-        logger.info(
-            f"Created {qrcodes_needed} new unclaimed QR codes to maintain "
+    #     logger.info(
+    #         f"Created {qrcodes_needed} new unclaimed QR codes to maintain "
             f"minimum of {MINIMUM_UNCLAIMED_QRCODES}. Current unclaimed: {unclaimed_count + qrcodes_needed}"
         )

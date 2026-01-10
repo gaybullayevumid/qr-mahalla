88a7caa8ea945584696a1e7acd33560ad9895c3d
gaybullayevumid <umidgaybullayev955@gmail.com>
2026-01-10 17:32:01 +0500

    Temporarily disable throttling settings for testing purposes

--- config/settings.py
+++ config/settings.py
@@ -80,14 +80,15 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
-    "DEFAULT_THROTTLE_CLASSES": [
-        "rest_framework.throttling.AnonRateThrottle",
-        "rest_framework.throttling.UserRateThrottle",
-    ],
-    "DEFAULT_THROTTLE_RATES": {
-        "anon": os.getenv("THROTTLE_ANON", "100/hour"),
-        "user": os.getenv("THROTTLE_USER", "1000/hour"),
-    },
+    # Temporarily disabled for testing
+    # "DEFAULT_THROTTLE_CLASSES": [
+    #     "rest_framework.throttling.AnonRateThrottle",
+    #     "rest_framework.throttling.UserRateThrottle",
+    # ],
+    # "DEFAULT_THROTTLE_RATES": {
+    #     "anon": os.getenv("THROTTLE_ANON", "100/hour"),
+    #     "user": os.getenv("THROTTLE_USER", "1000/hour"),
+    # },
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": int(os.getenv("PAGE_SIZE", "100")),
     "DEFAULT_RENDERER_CLASSES": [
